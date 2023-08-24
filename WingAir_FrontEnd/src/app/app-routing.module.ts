4943ff39811cbb52f14217063b2532914243a485
Guillermo Riaño <guilledark17@gmail.com>
2023-08-24 08:16:51 -0600

    ➕ Adding Routes

--- WingAir_FrontEnd/src/app/app-routing.module.ts
+++ WingAir_FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListarAeropuertosComponent } from './Aeropuertos/ListarAeropuertos/listar-aeropuertos/listar-aeropuertos.component';
+import { AgregarAeropuertoComponent } from './Aeropuertos/AgregarAeropuerto/agregar-aeropuerto/agregar-aeropuerto.component';
+import { DetallesAeropuertoComponent } from './Aeropuertos/DetallesAeropuerto/detalles-aeropuerto/detalles-aeropuerto.component';
+import { EditarAeropuertoComponent } from './Aeropuertos/EditarAeropuerto/editar-aeropuerto/editar-aeropuerto.component';
+import { ListarAvionesComponent } from './Aviones/ListarAviones/listar-aviones/listar-aviones.component';
+import { AgregarAvionComponent } from './Aviones/AgregarAvion/agregar-avion/agregar-avion.component';
+import { EditarAvionComponent } from './Aviones/EditarAvion/editar-avion/editar-avion.component';
+import { DetallesAvionComponent } from './Aviones/DetallesAvion/detalles-avion/detalles-avion.component';
+import { ListarPilotosComponent } from './Pilotos/ListarPilotos/listar-pilotos/listar-pilotos.component';
+import { AgregarPilotoComponent } from './Pilotos/AgregarPiloto/agregar-piloto/agregar-piloto.component';
+import { DetallesPilotoComponent } from './Pilotos/DetallesPiloto/detalles-piloto/detalles-piloto.component';
+import { EditarPilotoComponent } from './Pilotos/EditarPiloto/editar-piloto/editar-piloto.component';
+import { ListarVuelosComponent } from './Vuelos/ListarVuelos/listar-vuelos/listar-vuelos.component';
+import { AgregarVueloComponent } from './Vuelos/AgregarVuelo/agregar-vuelo/agregar-vuelo.component';
+import { DetallesVueloComponent } from './Vuelos/DetallesVuelo/detalles-vuelo/detalles-vuelo.component';
+import { EditarVueloComponent } from './Vuelos/EditarVuelo/editar-vuelo/editar-vuelo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ListarAeropuertos', component: ListarAeropuertosComponent},
+  { path: 'AgregarAeropuerto', component: AgregarAeropuertoComponent},
+  { path: 'DetallesAeropuerto/:id', component: DetallesAeropuertoComponent},
+  { path: 'EditarAeropuerto/:id', component: EditarAeropuertoComponent},
+  { path: 'ListarAviones', component: ListarAvionesComponent},
+  { path: 'AgregarAvion', component: AgregarAvionComponent},
+  { path: 'DetallesAvion/:id', component: DetallesAvionComponent},
+  { path: 'EditarAvion/:id', component: EditarAvionComponent},
+  { path: 'ListarPilotos', component: ListarPilotosComponent},
+  { path: 'AgregarPiloto', component: AgregarPilotoComponent},
+  { path: 'DetallesPiloto/:id', component: DetallesPilotoComponent},
+  { path: 'EditarPiloto/:id', component: EditarPilotoComponent},
+  { path: 'ListarVuelos', component: ListarVuelosComponent},
+  { path: 'AgregarVuelo', component: AgregarVueloComponent},
+  { path: 'DetallesVuelo/:id', component: DetallesVueloComponent},
+  { path: 'EditarVuelo/:id', component: EditarVueloComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WingAir_FrontEnd/src/app/app.module.ts
+++ WingAir_FrontEnd/src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarAeropuertosComponent } from './Aeropuertos/ListarAeropuertos/listar-aeropuertos/listar-aeropuertos.component';
+import { EditarAeropuertoComponent } from './Aeropuertos/EditarAeropuerto/editar-aeropuerto/editar-aeropuerto.component';
+import { AgregarAeropuertoComponent } from './Aeropuertos/AgregarAeropuerto/agregar-aeropuerto/agregar-aeropuerto.component';
+import { DetallesAeropuertoComponent } from './Aeropuertos/DetallesAeropuerto/detalles-aeropuerto/detalles-aeropuerto.component';
+import { ListarAvionesComponent } from './Aviones/ListarAviones/listar-aviones/listar-aviones.component';
+import { DetallesAvionComponent } from './Aviones/DetallesAvion/detalles-avion/detalles-avion.component';
+import { AgregarAvionComponent } from './Aviones/AgregarAvion/agregar-avion/agregar-avion.component';
+import { EditarAvionComponent } from './Aviones/EditarAvion/editar-avion/editar-avion.component';
+import { ListarPilotosComponent } from './Pilotos/ListarPilotos/listar-pilotos/listar-pilotos.component';
+import { AgregarPilotoComponent } from './Pilotos/AgregarPiloto/agregar-piloto/agregar-piloto.component';
+import { DetallesPilotoComponent } from './Pilotos/DetallesPiloto/detalles-piloto/detalles-piloto.component';
+import { EditarPilotoComponent } from './Pilotos/EditarPiloto/editar-piloto/editar-piloto.component';
+import { ListarVuelosComponent } from './Vuelos/ListarVuelos/listar-vuelos/listar-vuelos.component';
+import { DetallesVueloComponent } from './Vuelos/DetallesVuelo/detalles-vuelo/detalles-vuelo.component';
+import { AgregarVueloComponent } from './Vuelos/AgregarVuelo/agregar-vuelo/agregar-vuelo.component';
+import { EditarVueloComponent } from './Vuelos/EditarVuelo/editar-vuelo/editar-vuelo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarAeropuertosComponent,
+    EditarAeropuertoComponent,
+    AgregarAeropuertoComponent,
+    DetallesAeropuertoComponent,
+    ListarAvionesComponent,
+    DetallesAvionComponent,
+    AgregarAvionComponent,
+    EditarAvionComponent,
+    ListarPilotosComponent,
+    AgregarPilotoComponent,
+    DetallesPilotoComponent,
+    EditarPilotoComponent,
+    ListarVuelosComponent,
+    DetallesVueloComponent,
+    AgregarVueloComponent,
+    EditarVueloComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
